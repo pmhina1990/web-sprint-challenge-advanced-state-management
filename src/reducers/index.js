fb6daae94e1b3e58f7c45204c9b6a93329a21c90
Peter Benjamin Mhina <peterbenjaminmhina@Peters-MacBook-Pro.local>
2021-05-14 14:01:33 -0700

    Finished work

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,3 @@
-
-export const initialState = {
-}
-
-const reducer = ()=>{
-}
-
-export default reducer;
-
 //Task List:
 //1. Adds the following state values into the initialState:
 //  - an array of smurfs
@@ -19,3 +10,58 @@ export default reducer;
 //5. Add in a reducer cases to accomidate the failed smurf api fetch.
 //6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
 //7. Add in a reducer case that adds in a value to the error message.
+
+
+
+import { FETCH_SMURF_START, FETCH_SMURF_SUCCEEDED, FETCH_SMURF_FAILURE, ADD_SMURF, SMURF_ERROR } from '../actions/index'
+
+
+
+export const initialState = {
+    smurfs : [],
+    error: '',
+    isLoading :false
+}
+
+export const reducer = ( state = initialState, action)=>{
+
+    switch (action.type){
+        case(FETCH_SMURF_START) :
+        return({
+            ... state,
+            isLoading : true,
+        })
+
+        case (FETCH_SMURF_SUCCEEDED):
+            return({
+                ...state,
+                smurfs :action.payload,
+                isLoading :false,
+                error: '',
+            })
+
+            case (FETCH_SMURF_FAILURE):
+                return({
+                    ...state,
+                    isLoading :true,
+                    error : "Yikes! Smurf is Not Here,Try Again"+ action.payload,
+
+                })
+                case (ADD_SMURF):
+                    return({
+                        ...state,
+                        smurfs:[...state.smurfs,action.payload],
+                        isLoading : false
+                    })
+                    case (SMURF_ERROR):
+                        return({
+                            isLoading: true,
+                            error : action.payload
+                        })
+                        default : return state
+        }
+    };
+
+
+export default reducer;
+
